e2b7468c249530d81df6939953df053078863d0c
sdaviswalmart <sdavi11@l-sb8j552g8w-m.homeoffice.wal-mart.com>
2016-06-13 13:15:56 -0700

    Initial Commit

---
+++ HW5 - Facebook/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HW5 - Facebook
+//
+//  Created by Stephen Davis on 6/13/16.
+//  Copyright © 2016 Stephen Davis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
